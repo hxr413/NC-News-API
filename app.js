42d46723c77240b8564cb383d72fabb6612e0770
Riley Hu <hxr413@gmail.com>
2023-08-15 11:33:35 +0100

    Refactor error handling

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const { getDoc, getTopics } = require("./controllers/topics-controllers");
-const { getArticles, getArticleById } = require("./controllers/articles-controllers");
-const { getCommentsById } = require("./controllers/comments-controllers");
+const {
+  getArticles,
+  getArticleById,
+} = require("./controllers/articles-controllers");
+const {
+  getCommentsById,
+  postCommentById,
+} = require("./controllers/comments-controllers");
 
 app.get("/api", getDoc);
 
@@ -13,6 +19,11 @@ app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleById);
 
 app.get("/api/articles/:article_id/comments", getCommentsById);
+app.post("/api/articles/:article_id/comments", postCommentById);
+
+app.use((request, response) => {
+  response.status(404).send({ message: "path does not exist" });
+});
 
 app.use((err, request, response, next) => {
   // console.log(err, "err in app");
