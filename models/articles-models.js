3fa99080eb6ac921b27941f14c8c107915ce9117
Riley Hu <hxr413@gmail.com>
2023-08-16 09:53:56 +0100

    Complete task 12

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 
 exports.selectArticles = (sort, order) => {
-  const selectQuery = `SELECT articles.article_id, title, topic, author, created_at, votes, article_img_url, COALESCE(comments.comment_count, 0) AS comment_count
+  const selectQuery = `SELECT articles.article_id, title, topic, author, created_at, votes, article_img_url, COALESCE(comments.comment_count::integer, 0) AS comment_count
   FROM articles LEFT JOIN (
     SELECT article_id, COUNT(*) AS comment_count 
     FROM comments GROUP BY article_id
@@ -15,15 +15,20 @@ exports.selectArticles = (sort, order) => {
 };
 
 exports.selectArticleById = (id) => {
-  return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [id])
-    .then((result) => {
-      const output = result.rows[0];
-      if (!output) {
-        return Promise.reject({ status: 404, msg: "article does not exist" });
-      }
-      return output;
-    });
+  const selectQuery = `SELECT articles.article_id, title, topic, author, body, created_at, votes, article_img_url, COALESCE(comments.comment_count::integer, 0) AS comment_count
+  FROM articles LEFT JOIN (
+    SELECT article_id, COUNT(*) AS comment_count 
+    FROM comments GROUP BY article_id
+    ) comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1;`;
+  return db.query(selectQuery, [id]).then((result) => {
+    const output = result.rows[0];
+    // console.log(id, "id", result.rows,"output in model")
+    if (!output) {
+      return Promise.reject({ status: 404, msg: "article does not exist" });
+    }
+    return output;
+  });
 };
 
 exports.updateArticleById = (votesChange, id) => {
