b4db66d969909a881c6529fa13a453363c5b724e
Riley Hu <hxr413@gmail.com>
2023-09-04 09:54:50 +0100

    Add cors for front end project

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const cors = require('cors');
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-routers");
+
+app.use(cors());
 
 app.use(express.json());
 
